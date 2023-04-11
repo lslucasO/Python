f3d1c2db0d9818c6aab8013122a32303223129a1
Lucas Santana Lemos <lslemos59@gmail.com>
2023-04-11 15:00:04 -0300

    adjustin again

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -1,18 +1,25 @@
 import discord, key
 from discord.ext import commands
 
-
 intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-
-DISCORD_API_KEY = key.token
-
-
-@client.event
-async def on_ready():
-    print(f'{client.user} esta online!')
+intents.message_content = True # <-- Intents basicos
+ 
+DISCORD_API_KEI = key.token # <-- Key do bot
 
 
+class MyClient(discord.Client):  # <-- Class principal do bot
 
-client.run(DISCORD_API_KEY)
+    async def on_ready(self): 
+        print(f'{self.user} esta online!')   
+
+    
+    async def on_message(self, message):
+        if message.content.startswith('!oi'):
+            print(f'oi cara')
+
+        print(f'Message from {message.author}: {message.content} ')
+
+        
+
+client = MyClient(intents=intents)
+client.run(DISCORD_API_KEI)
