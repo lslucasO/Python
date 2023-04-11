0607e551046b8627f97f4dffc3e27a57334008be
Lucas Santana Lemos <lslemos59@gmail.com>
2023-04-11 14:01:38 -0300

    iniciando

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -1,5 +1,18 @@
-import discord
+import discord, key
 from discord.ext import commands
 
-client = discord.Client()
 
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+DISCORD_API_KEY = key.token
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} esta online!')
+
+
+
+client.run(DISCORD_API_KEY)
